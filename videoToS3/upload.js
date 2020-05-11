bda85fc3ec04af58d539f780f57b0ded270f9b12
Greg <gwprill@gmail.com>
2020-05-10 17:11:57 -0700

    Git passwords out of repo

--- videoToS3/upload.js
+++ videoToS3/upload.js
@@ -1,10 +1,10 @@
-process.env.DB_HOSTNAME = 'mbt.czt7i8bu37g9.us-east-2.rds.amazonaws.com'
-process.env.DB_USERNAME = 'admin'
-process.env.DB_PASSWORD = 'tR5_XtBjvdR3XWvy'
-process.env.DB_NAME = 'mbt'
+process.env.DB_HOSTNAME = '#'
+process.env.DB_USERNAME = '#'
+process.env.DB_PASSWORD = '#'
+process.env.DB_NAME = '#'
 
-process.env.AWS_ACCESS_KEY = '###';
-process.env.AWS_SECRET_ACCESS_KEY = '###';
+process.env.AWS_ACCESS_KEY = '#';
+process.env.AWS_SECRET_ACCESS_KEY = '#';
 
 process.env.bucketName = 'videos-youtube-2'
 
--- youtubeToMySQL/sync.js
+++ youtubeToMySQL/sync.js
@@ -1,10 +1,10 @@
-process.env.API_TOKEN = 'AIzaSyDPKlpdOuQWvUf4bf-z2WIpLD7v5WuFYdw';
-process.env.playlistId = 'UUYwlraEwuFB4ZqASowjoM0g';
+process.env.API_TOKEN = '#';
+process.env.playlistId = '#';
 
-process.env.DB_HOSTNAME = 'mbt.czt7i8bu37g9.us-east-2.rds.amazonaws.com'
-process.env.DB_USERNAME = 'admin'
-process.env.DB_PASSWORD = 'tR5_XtBjvdR3XWvy'
-process.env.DB_NAME = 'mbt'
+process.env.DB_HOSTNAME = '#'
+process.env.DB_USERNAME = '#'
+process.env.DB_PASSWORD = '#'
+process.env.DB_NAME = '#'
 
 const index = require('./index');
 
